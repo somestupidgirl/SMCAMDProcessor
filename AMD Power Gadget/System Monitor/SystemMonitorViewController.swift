fc12cc875dd08fa52a8cfd129ba266cd408f07e6
cgrazy <66645171+cgrazy@users.noreply.github.com>
2021-03-06 15:18:55 +0100

    Color adaptations and fan control window handling
    Color adaptations, and fix that Fan Control windows opens multiple times, #121

--- AMD Power Gadget/System Monitor/SystemMonitorViewController.swift
+++ AMD Power Gadget/System Monitor/SystemMonitorViewController.swift
@@ -8,21 +8,21 @@
 
 import Cocoa
 
-class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource, NSWindowDelegate {
 
     var timer : Timer?
-    
+
     var numFans = 0
     var fanNames : [String] = []
     var driverLoaded = false
     var fanRpms : [UInt64] = []
     var fanThrottles : [UInt8] = []
     var fanOverrided : [Bool] = []
-    
+
     @IBOutlet weak var tableView: NSTableView!
-    
+
     @IBOutlet weak var chipIntelLabel: NSTextField!
-    
+
     static var activeSelf : SystemMonitorViewController?
     static func launch(forceFocus: Bool = false) {
         if let vc = SystemMonitorViewController.activeSelf {
@@ -31,20 +31,20 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
             let mainStoryboard = NSStoryboard.init(name: NSStoryboard.Name("Main"), bundle: nil)
             let controller = mainStoryboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("SystemMonitor")) as! NSWindowController
             controller.showWindow(self)
-            
+            controller.window?.isMovableByWindowBackground = true
             if forceFocus {controller.window?.orderFrontRegardless()}
         }
     }
-    
-    
-    
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //init smc
         let initRes = ProcessorModel.shared.kernelGetUInt64(count: 2, selector: 90)
         if initRes[0] != 1{
-            
+
             let alert = NSAlert()
             alert.messageText = "Could not found driver for your SMC."
             alert.informativeText = "Your SMC chip is likely not supported yet."
@@ -52,33 +52,33 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
             alert.addButton(withTitle: "Done")
             alert.addButton(withTitle: "Report Issue")
             let res = alert.runModal()
-            
+
             if res == .alertSecondButtonReturn {
                 NSWorkspace.shared.open(URL(string: "https://github.com/trulyspinach/SMCAMDProcessor/issues/66")!)
             }
-            
+
         } else {driverLoaded = true}
-        
+
         ProcessorModel.shared.fetchSMCChipSupport(chipIntel: Int(initRes[1]), working: initRes[0] == 1)
-        
+
         chipIntelLabel.stringValue =
             String(format: "Chip ID: %X, Revision: %X, \(driverLoaded ? "Connected" : "Not yet supported")",
                     initRes[1] >> 8, initRes[1] & 0xff)
-        
+
         if driverLoaded {
             numFans = Int(ProcessorModel.shared.kernelGetUInt64(count: 1, selector: 91)[0])
-            
+
             for i in 0...numFans-1 {
                 fanNames.append(ProcessorModel.shared.kernelGetString(selector: 92, args: [UInt64(i)]))
             }
-            
+
             fanRpms = [UInt64](repeating: 0, count: numFans)
             fanThrottles = [UInt8](repeating: 0, count: numFans)
             fanOverrided = [Bool](repeating: false, count: numFans)
-            
+
             timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { (_) in
                 self.updateFanRPMs()
-                
+
                 if let t = ProcessorModel.shared.getThrottle() {
                     for i in 0..<self.numFans {
                         ProcessorModel.shared.kernelSetUInt64(selector: 95, args: [UInt64(i), UInt64(t)])
@@ -87,52 +87,61 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
             })
         }
 
-        
+
         tableView.dataSource = self
         tableView.delegate = self
-        
+
         tableView.sizeToFit()
-        
+
         SystemMonitorViewController.activeSelf = self
         AppDelegate.updateDockIcon()
     }
-    
+
     override func viewWillAppear() {
         view.window?.delegate = self
     }
-    
+
+    override func viewDidAppear() {
+            self.view.window?.delegate = self
+    }
+
+    func windowWillClose(_ notification: Notification) {
+        SystemMonitorViewController.activeSelf = nil
+        AppDelegate.updateDockIcon()
+    }
+
     func updateFanRPMs() {
         fanRpms = ProcessorModel.shared.kernelGetUInt64(count: numFans, selector: 93)
         tableView.reloadData()
-        
+
         let ctrls = ProcessorModel.shared.kernelGetUInt64(count: numFans, selector: 94)
         for i in 0...numFans-1 {
             fanThrottles[i] = UInt8(ctrls[i] >> 8)
             fanOverrided[i] = (ctrls[i] & 0xff) == 0
         }
     }
-    
+
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
         return 30
     }
-    
+
     func numberOfRows(in tableView: NSTableView) -> Int {
         return numFans
     }
-    
+
     @IBAction func autobutton(_ sender: Any) {
         if ProcessorModel.shared.kernelSetUInt64(selector: 97, args: [0]) {
             updateFanRPMs()
         }
     }
-    
+
     @IBAction func takeOffButton(_ sender: Any) {
         if ProcessorModel.shared.kernelSetUInt64(selector: 97, args: [1]) {
             updateFanRPMs()
         }
     }
-    
-    
+
+
     func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
         
         if tableColumn!.identifier.rawValue == "fan" {
@@ -153,8 +162,8 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
         
         return 0
     }
-    
-    
+
+
     func tableView(_ tableView: NSTableView, setObjectValue object: Any?, for tableColumn: NSTableColumn?, row: Int) {
         if tableColumn!.identifier.rawValue == "control" {
             let thr = UInt8(round(Double(truncating: object as! NSNumber)))
@@ -162,7 +171,7 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
                 updateFanRPMs()
             }
         }
-        
+
         if tableColumn!.identifier.rawValue == "override" {
             let v = object as! Bool
             
@@ -170,12 +179,5 @@ class SystemMonitorViewController: NSViewController, NSTableViewDelegate, NSTabl
                 updateFanRPMs()
             }
         }
-    }
-}
-
-extension SystemMonitorViewController: NSWindowDelegate {
-    func windowWillClose(_ notification: Notification) {
-        SystemMonitorViewController.activeSelf = nil
-        AppDelegate.updateDockIcon()
     }
 }
